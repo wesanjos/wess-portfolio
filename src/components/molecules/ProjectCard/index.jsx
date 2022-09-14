2e5e5f0cbcbb145aa4cd3a17c2e4a88d390b6948
Wesley Anjos <wanjos.andrade@gmail.com>
2022-09-13 21:16:07 -0300

    Link de visualizar projeto

--- src/components/molecules/ProjectCard/index.jsx
+++ src/components/molecules/ProjectCard/index.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Image from 'next/image';
 import { useParallax } from 'react-scroll-parallax';
 import TechList from '../TechList';
 
-/* eslint-disable jsx-a11y/alt-text */
 export default function ProjectCard({
   projectImage,
   projectName,
@@ -13,22 +13,33 @@ export default function ProjectCard({
 
   return (
     <div className="projectCard" ref={ref}>
-      <div className="projectCard__overlay" />
-
       <div className="projectCard__container">
         <Image
+          className="projectCard__image"
           src={projectImage}
           width="430"
           height="650"
           objectFit="contain"
         />
+      </div>
 
-        <div className="projectCard__header">
+      <div className="projectCard__header">
+        <div className="headerCard__container">
           <h4>{projectName}</h4>
           <span>{projectDescription}</span>
 
           <TechList techList={techList} />
         </div>
+
+        <a
+          href="#"
+          target="_blank"
+          rel="noreferrer"
+          className="projectCard__link"
+        >
+          Visualizar site
+          <img src="/assets/icons/icon-link.svg" alt="Link de visualização" />
+        </a>
       </div>
     </div>
   );
