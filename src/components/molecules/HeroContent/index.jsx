fb4f5d57e2662b6b1254037ef181c8f18d2691c7
Wesley Anjos <wanjos.andrade@gmail.com>
2023-07-15 10:49:29 -0300

    feat: correção do tamanho da imagem principal

--- src/components/molecules/HeroContent/index.jsx
+++ src/components/molecules/HeroContent/index.jsx
@@ -27,6 +27,7 @@ export default function HeroContent({ anchor, heroInfo, photoInfo }) {
           height={photoInfo.photo.height}
           alt={photoInfo.photo.alt}
           priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
     </div>
