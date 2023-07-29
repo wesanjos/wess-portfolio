46bdacbe0217d184c49aa827210cde06f3a9f455
Wesley Anjos <wanjos.andrade@gmail.com>
2023-07-29 12:11:20 -0300

    feat: inclusão do cv

--- src/components/molecules/ContactList/index.jsx
+++ src/components/molecules/ContactList/index.jsx
@@ -22,6 +22,23 @@ export default function ContactList({ currentContacts }) {
           </a>
         </li>
       ))}
+      <li className="contact-list__item">
+        <a
+          className="contact-list__link"
+          href="assets/pdf/WesleyAnjos_CV.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="assets/icons/icon-resume.svg"
+            alt="Visualizar projeto"
+            width={26}
+            height={26}
+            loading="lazy"
+          />
+          Currículo
+        </a>
+      </li>
     </ul>
   );
 }
